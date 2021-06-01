d73a34a29eb7cf06e244b9f4c121db8c02a01429
__muff <trent.sch02@mail.com>
2021-06-01 13:37:30 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# About
+
+A python script to help automate project creation
+
+## Install
+
+Run the following commands:
+
+```pip install requirements .txt```
+
+## Instructions
+
+```python3 ./create.py [project_name]```
+
+### Side notes
+
+You could create a simple bash script to further optimize the rate in which you use the script.
---
+++ src/create.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import sys, os, git
+    
+def create_files(dir_name):
+    """[Creates files and directories]
+
+    Args:
+        dir_name ([str]): [The directory that is created]
+    """
+    
+    # Attempts to create director, returns if it already exists
+    try:
+        os.mkdir(dir_name)
+        print("Directory created: " + dir_name)
+    except OSError as error: 
+        print(error)
+        return
+    
+    # Init new git repo
+    repo = git.Repo.init(dir_name)
+    print("Initalized git repo")
+    
+    # cd ./[dir_name]
+    os.chdir(dir_name)
+    
+    # Add default directories
+    os.mkdir("src")
+    os.mkdir("resources")
+    os.mkdir("test")
+    print("Directories created: src, resources, test")
+    
+    # Create the .gitignore and README.md files
+    if(os.path.exists("./" + dir_name + ".gitignore") == False):
+        GIT_IGNORE = open(".gitignore", "x")
+        print("Created: .gitignore")
+        
+    if (os.path.exists("./" + dir_name + ".README.md") == False):
+        README = open("README.md", "x")
+        print("Created: README.md")
+    
+    # Files to stage 
+    repo.index.add(['.gitignore', 'README.md'])
+    print("Staged files: " + ".gitignore, README.md")
+    
+    # Initial commit 
+    repo.index.commit('Initial commit.')
+    print("Committed files.")
+    
+def main():
+    """[Script main function]
+    """
+    
+    # Check if they provide arguemnts
+    if(len(sys.argv) != 2):
+        print("Please specify the directory name.")
+        return
+    
+    create_files(sys.argv[1])
+        
+# Calls main function
+main()
